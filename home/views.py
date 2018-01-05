70a826c3f8ad7e79723087c2a0607d4e13b8624a
Igordr1999 <Igordr1999@yandex.ru>
2018-01-05 19:26:11 +0300

    update few templates

--- home/views.py
+++ home/views.py
@@ -8,15 +8,15 @@ def home(request):
 
 def blog(request):
     all_articles = BlogArticle.objects.all()
-    return render(request, 'blog.html', {"all_articles": all_articles})
+    return render(request, 'blog/blog.html', {"all_articles": all_articles})
 
 
 def terms(request):
-    return render(request, 'terms.html', {})
+    return render(request, 'site_info/terms.html', {})
 
 
 def personal_data_policy(request):
-    return render(request, 'personal_data_policy.html', {})
+    return render(request, 'site_info/personal_data_policy.html', {})
 
 
 def blog_article(request, num):
